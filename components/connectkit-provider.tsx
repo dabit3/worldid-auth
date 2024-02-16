95246dd75fccd556aec492ace830d5f24a5407bf
nader <dabit3@gmail.com>
2024-02-16 12:21:21 -0600

    upgrade connectkit, wagmi, viem

--- components/connectkit-provider.tsx
+++ components/connectkit-provider.tsx
@@ -1,21 +1,25 @@
 'use client'
-import { WagmiConfig, createConfig } from "wagmi";
+import { WagmiProvider, createConfig } from "wagmi";
 import { ConnectKitProvider as Provider, getDefaultConfig } from "connectkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const config = createConfig(
   getDefaultConfig({
-    alchemyId: process.env.ALCHEMY_ID,
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
     appName: "WorldCoin ID Connect",
   }),
 );
 
+const queryClient = new QueryClient();
+
 export const ConnectKitProvider = ({ children }: { children: any }) => {
   return (
-    <WagmiConfig config={config}>
-      <Provider>
-        {children }
-      </Provider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          {children }
+        </Provider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
